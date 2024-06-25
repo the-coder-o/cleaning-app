a94644085151ba64c87702734bdc7daee05874bc
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-06-25 17:06:06 +0500

    finished project

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,15 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <head>
         <title>Hello World</title>
         <ChatWood />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body className={`${montserrat.variable} ${asap.variable} `}>
         <Header />
         {children}
         <Footer />
+        <a href={"tel:(804) 852-8030"} className={"fixed bottom-24 right-8 flex h-[50px] w-[50px] items-center justify-center rounded-full bg-[#9BCC11] "}>
+          <i className="fa-solid fa-phone fa-shake text-white"></i>
+        </a>
       </body>
     </html>
   );

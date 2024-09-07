b3e4dc1b81103d03c97dcb0818e591f704d2d394
the-coder-o <abdulbositsharipovv@gmail.com>
2024-09-07 17:21:25 +0500

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import { Montserrat, Asap } from "next/font/google";
 import Header from "@/components/header/header";
 import "./globals.css";
-import ChatWood from "@/chat/chat";
-import Footer from "@/components/footer/footer";
+import LiveChat from "@/chat/chat";
 const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -19,7 +18,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="en">
       <head>
         <title>Hello World</title>
-        <ChatWood />
+        <LiveChat />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body className={`${montserrat.variable} ${asap.variable}`}>

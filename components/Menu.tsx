9b95ecabcd3aba932ee665f6ac4ce43918200188
the-coder-o <abdulbositsharipovv@gmail.com>
2024-09-07 17:18:39 +0500

    fix

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,43 +1,43 @@
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { IoMdMenu } from "react-icons/io";
 import { Dialog, DialogClose, DialogTrigger } from "./ui/dialog";
 import Modal from "./Modal";
-import { Link } from 'react-scroll'
+import { Link } from "react-scroll";
 
 export function SheetDemo() {
   return (
     <div className="">
-      <ul className=' gap-3 hidden md:flex'>
-        <Link to="services" spy={true} smooth={true} offset={-120} duration={500}>Services</Link>
+      <ul className=" hidden gap-3 md:flex">
+        <Link to="services" spy={true} smooth={true} offset={-120} duration={500}>
+          Services
+        </Link>
         <Dialog>
           <DialogTrigger>Contact us</DialogTrigger>
           <Modal />
         </Dialog>
-        <a href='tel:(877) 340 - 1327' className=' font-semibold text-[10px] sm:text-sm md:text-base lg:text-base '>(877) 340 - 1327</a>
+        <a href="tel:(757) 272-9870" className=" text-[10px] font-semibold sm:text-sm md:text-base lg:text-base ">
+          (757) 272-9870
+        </a>
       </ul>
       <div className="flex md:hidden ">
         <Sheet>
           <SheetTrigger asChild>
             <IoMdMenu className="cursor-pointer" />
           </SheetTrigger>
-          <SheetContent className="flex bg-white text-black flex-col items-center text-center">
+          <SheetContent className="flex flex-col items-center bg-white text-center text-black">
             <SheetHeader className="mt-12">
               <SheetTitle>
                 <DialogClose asChild>
-                  <Link to="services" spy={true} smooth={true} offset={-70} duration={500}>Services</Link>
+                  <Link to="services" spy={true} smooth={true} offset={-70} duration={500}>
+                    Services
+                  </Link>
                 </DialogClose>
               </SheetTitle>
             </SheetHeader>
             <SheetHeader>
               <SheetTitle>
                 <Dialog>
-                  <DialogTrigger className='flex text-[10px] items-start rounded-full  px-6 py-1 md:py-2 md:px-5 font-semibold text-center text-white bg-gradient-to-r from-indigo-500 to-purple-600 focus:bg-indigo-700 focus:outline-none'>Contact us</DialogTrigger>
+                  <DialogTrigger className="flex items-start rounded-full bg-gradient-to-r  from-indigo-500 to-purple-600 px-6 py-1 text-center text-[10px] font-semibold text-white focus:bg-indigo-700 focus:outline-none md:px-5 md:py-2">Contact us</DialogTrigger>
                   <Modal />
                 </Dialog>
               </SheetTitle>
@@ -46,5 +46,5 @@ export function SheetDemo() {
         </Sheet>
       </div>
     </div>
-  )
+  );
 }
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,32 +1,32 @@
-'use client'
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-import logo from '../../images/png/logo.png'
-import Link from 'next/link';
-import { SheetDemo } from '../Menu';
-
+"use client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import logo from "../../images/png/logo.png";
+import Link from "next/link";
+import { SheetDemo } from "../Menu";
 
 const Header = () => {
-
-  const [navbar, setNavbar] = useState('py-4')
+  const [navbar, setNavbar] = useState("py-4");
   useEffect(() => {
     const scrollHandler = () => {
-      const changeNav = window.pageYOffset > 100 ? 'py-4' : 'py-2';
-      setNavbar(changeNav)
+      const changeNav = window.pageYOffset > 100 ? "py-4" : "py-2";
+      setNavbar(changeNav);
     };
-    window.addEventListener('scroll', scrollHandler);
+    window.addEventListener("scroll", scrollHandler);
 
-    return window.removeEventListener('scroll', scrollHandler)
-  }, [])
+    return window.removeEventListener("scroll", scrollHandler);
+  }, []);
   return (
-    <div className='fixed px-3 w-full py-3 backdrop-blur-xl bg-white/70 z-20  flex items-center justify-between '>
-      <Link href={'/'} className='flex items-center gap-1 md:gap-2'>
-        <Image alt='logo' width={120} height={120} src={logo} />
+    <div className="fixed z-20 flex w-full items-center justify-between bg-white/70  px-3 py-3 backdrop-blur-xl ">
+      <Link href={"/"} className="flex items-center gap-1 md:gap-2">
+        <Image alt="logo" width={120} height={120} src={logo} />
       </Link>
-      <a href='tel:(877) 340 - 1327' className='block md:hidden font-semibold montserrat mr-20 text-[10px] sm:text-sm md:text-base lg:text-lg xl:text-xl'>(877) 340 - 1327</a>
+      <a href="tel:(757) 272-9870" className="montserrat mr-20 block text-[10px] font-semibold sm:text-sm md:hidden md:text-base lg:text-lg xl:text-xl">
+        (757) 272-9870
+      </a>
       <SheetDemo />
     </div>
-  )
+  );
 };
 
 export default Header;
